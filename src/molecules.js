47c45352f145bf89ecfb008379043bf4583339ed
Andrew White <white.d.andrew@gmail.com>
2023-04-03 11:58:05 -0400

    added antrhopic and cohere

--- src/molecules.js
+++ src/molecules.js
@@ -685,5 +685,421 @@ export default [{
     "result": "CC(=O)Oc1ccccc1C(=O)OC",
     "type": "sub",
     "model": "text-davinci-003"
-}
+},
+    {
+     "left": "CC(C)(C)c1ccc(O)c(c1)C(C)(C)C",
+     "result": [
+      "CN1C=NC2=C1C(=O)N(C(=O)N2C)C",
+      "Cc1cccc(C)c1OCc1ccc(C(C)C)cc1",
+      "CC(C)(C)c1cncc(C)c1O",
+      "Cc1ccccc1C(C)(C)CO",
+      "CC(C)(C)CCC(=O)N1CCN(CC1)C\\C=C/c1ccccc1"
+     ],
+     "model": "claude-v1",
+     "type": "mutate"
+    },
+    {
+     "left": "Cc1cccc(C=C)c1",
+     "result": [
+      "C=CCc1ccccc1",
+      "C/C(=C\\C)c1ccccc1",
+      "C=C\\C=C/c1ccccc1",
+      "CC(=C)C(C=C)c1ccccc1",
+      "C/C(=C/C)c1ccccc1"
+     ],
+     "model": "claude-v1",
+     "type": "mutate"
+    },
+    {
+     "left": "O=C(C)Oc1ccccc1C(=O)O",
+     "result": [
+      "Cn1c(=O)c2ccccc12",
+      "COc1ccc(C(=O)O)cc1",
+      "CN1C(=O)c2ccccc12",
+      "CC(C)=C\\C(=O)O/c1ccccc1",
+      "O=Cc1ccc(C(=O)O)cc1"
+     ],
+     "model": "claude-v1",
+     "type": "mutate"
+    },
+    {
+     "left": "C[Si]1(C)C=C2OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12",
+     "result": [
+      "C1=C=C(CC=C2OCCOC2=NC(=NC(=O)C3CCOC(C(=O)C4=CC=CC=C4)=C3)F)SC1",
+      "C1=C=C(CC=C2OCCOC2=NC=NC(=O)C(=O)COC3=CC=CC=C3)SC1"
+     ],
+     "model": "claude-v1",
+     "type": "mutate"
+    },
+    {
+     "left": "CC(C)(C)c1ccc(O)c(c1)C(C)(C)C",
+     "right": "Cc1cccc(C=C)c1",
+     "result": "C[C@]12CC[C@H]3[C@H]2C[C@](C)(C)[C@@H](OC4=CC=CC(=C4)C(C)(C)C)[C@@H](C)[C@]4(C)CC[C@@H]5[C@@H]4CC[C@@H]5C3",
+     "type": "add",
+     "model": "claude-v1"
+    },
+    {
+     "left": "CC(C)(C)c1ccc(O)c(c1)C(C)(C)C",
+     "right": "Cc1cccc(C=C)c1",
+     "result": "CC(C)(C)c1ccc(O)c(c1)C(C)(C)CC=C",
+     "type": "add",
+     "model": "claude-v1"
+    },
+    {
+     "left": "C=CCC(C)(C)c1cc(C(C)(C)C)ccc1O",
+     "right": "Cc1cccc(C=C)c1",
+     "result": "CCCCCCCC(C)(C)c1cc(C(C)(C)C)ccc1O - Cc1cccc(C=C)c1\nCc1cccc(C)c1",
+     "type": "sub",
+     "model": "claude-v1"
+    },
+    {
+     "left": "CC(C)(C)c1ccc(O)c(c1)C(C)(C)C",
+     "right": "Cc1ccc(cc1Nc2nccc(n2)c3cccnc3)NC(=O)c4ccc(cc4)CN5CCN(CC5)C",
+     "result": "CC(C)(C)c1ccc(O)c(c1)C(C)(C)Cc6ccc(cc6Nc7nccc(n7)c8cccnc8)NC(=O)c9ccc(cc9)CN%10CCN(CC10)C",
+     "type": "add",
+     "model": "claude-v1"
+    },
+    {
+     "left": "CC(C)(C)c1ccc(O)c(c1)C(C)(C)C",
+     "right": "O=C(C)Oc1ccccc1C(=O)O",
+     "result": "CC(C)(Oc1ccccc1C(=O)O)c1ccc(O)c(c1)C(C)(C)C",
+     "type": "add",
+     "model": "claude-v1"
+    },
+    {
+     "left": "CC(C)(C)c1cc(C(C)(C)Oc2ccccc2C(=O)O)ccc1O",
+     "right": "O=C(C)Oc1ccccc1C(=O)O",
+     "result": "O=C(C)Oc1ccc(C(C)(C)Oc2ccccc2C(=O)O)cc1",
+     "type": "sub",
+     "model": "claude-v1"
+    },
+    {
+     "left": "CC(C)(C)c1ccc(O)c(c1)C(C)(C)C",
+     "right": "C[Si]1(C)C=C2OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12",
+     "result": "CC(C)(C)c1ccc(O)c(c1)C(C)(C)CNC(=O)C1=C[Si]2(C)CCOC2=C1C1=CC=C(c2oc3ccccc3c2)C2N=C(F)C(F)=NC12",
+     "type": "add",
+     "model": "claude-v1"
+    },
+    {
+     "left": "CC(C)(C)c1ccc(O)c(C(C)(C)CNC(=O)C2=C[Si]3(C)CCOC3=C2C2=CC=C(c3cc4ccccc4o3)C3N=C(F)C(F)=NC23)c1",
+     "right": "C[Si]1(C)C=C2OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12",
+     "result": "C1=CC=C(C2=C3OCCOC3=C[Si](C)C2CCC(=O)NC(C)(C)C)C([Si](C)CC(O)(C(C)(C)CNC(=O)C2=C[Si](C)CCOC2=C3C=CC=C3c2cc3ccccc3o2)c1)C2N=C(F)C(F)=NC12",
+     "type": "sub",
+     "model": "claude-v1"
+    },
+    {
+     "left": "Cc1cccc(C=C)c1",
+     "right": "Cc1ccc(cc1Nc2nccc(n2)c3cccnc3)NC(=O)c4ccc(cc4)CN5CCN(CC5)C",
+     "result": "CCN1CCN(CC1)C(=O)Nc2ccc(cc2)c3nccc(n3)Nc4ccc(cc4=C)C=C",
+     "type": "add",
+     "model": "claude-v1"
+    },
+    {
+     "left": "Cc1cccc(C=C)c1",
+     "right": "O=C(C)Oc1ccccc1C(=O)O",
+     "result": "CC(=O)Oc1cccc(C=CCc2ccccc2)c1C(=O)O",
+     "type": "add",
+     "model": "claude-v1"
+    },
+    {
+     "left": "CC(=O)Oc1cccc(C=CCc2ccccc2)c1C(=O)O",
+     "right": "O=C(C)Oc1ccccc1C(=O)O",
+     "result": "O=C(C)Oc1ccc(/C=CCc2ccccc2)cc1C(=O)O",
+     "type": "sub",
+     "model": "claude-v1"
+    },
+    {
+     "left": "Cc1cccc(C=C)c1",
+     "right": "C[Si]1(C)C=C2OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12",
+     "result": "Nc1c(F)c(NC2=C(C=CC=C1C3OCCOC3=CC=C2C)C4OCCOC4=CC=CC(C)=C)C(F)=N",
+     "type": "add",
+     "model": "claude-v1"
+    },
+    {
+     "left": "Cc1ccc(cc1Nc2nccc(n2)c3cccnc3)NC(=O)c4ccc(cc4)CN5CCN(CC5)C",
+     "right": "O=C(C)Oc1ccccc1C(=O)O",
+     "result": "CN1CCN(CC1)c2ccccc2NC(=O)c3cccc(c3)Nc4nccc(n4)c5ccc(NC)cc5",
+     "type": "add",
+     "model": "claude-v1"
+    },
+    {
+     "left": "CNc1ccc(-c2ccnc(Nc3cccc(C(=O)Nc4ccccc4N4CCN(C)CC4)c3)n2)cc1",
+     "right": "O=C(C)Oc1ccccc1C(=O)O",
+     "result": "CNc1ccc(c2ccnc(Nc3cccc(N4CCN(C)CC4)c3)n2)cc1",
+     "type": "sub",
+     "model": "claude-v1"
+    },
+    {
+     "left": "Cc1ccc(cc1Nc2nccc(n2)c3cccnc3)NC(=O)c4ccc(cc4)CN5CCN(CC5)C",
+     "right": "C[Si]1(C)C=C2OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12",
+     "result": "CC1=C[C+]([C+]([OC]([C+]([OC]([C+]([OC]([C+]1C)C)(CC2=C3NC(=O)C(=C4N=C(F)C(=C4)C)CC(C3CCC2)N(C)C)CC3=C([Si]([C]([C#N]4CC(C4)C)(C)[C]3=C(C)C)C)C)C)C)C",
+     "type": "add",
+     "model": "claude-v1"
+    },
+    {
+     "left": "O=C(C)Oc1ccccc1C(=O)O",
+     "right": "C[Si]1(C)C=C2OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12",
+     "result": "O=C(C)Oc1ccccc1C(=O)OCC1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12",
+     "type": "add",
+     "model": "claude-v1"
+    },
+    {
+     "left": "CC(=O)Oc1ccccc1C(=O)OCC1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12",
+     "right": "C[Si]1(C)C=C2OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12",
+     "result": "CN1C=NC(=C1F)C1=CC=C(C2=C3OCCOC3=CC2)",
+     "type": "sub",
+     "model": "claude-v1"
+    },
+    {
+        "left": "CC(C)(C)c1ccc(O)c(c1)C(C)(C)C",
+        "result": [
+         "CC(C)(C)CCC(C)(C)C",
+         "C(CCC)(CCC)C",
+         "CCC(=O)CN(C)C(C)(C)C",
+         "O=CCC(NC(C)=O)Nc1ccc(Cl)cc1",
+         "CCC(F)C",
+         "CC(F)C"
+        ],
+        "model": "claude-instant-v1",
+        "type": "mutate"
+       },
+       {
+        "left": "Cc1cccc(C=C)c1",
+        "result": [
+         "Cc1ccc(F)cc1",
+         "CCCCC=C",
+         "CC(O)=NC1=CNC=N1",
+         "CCCN(CCC(C)C)Cl",
+         "CCCN(CCC(C)C)CCC"
+        ],
+        "model": "claude-instant-v1",
+        "type": "mutate"
+       },
+       {
+        "left": "O=C(C)Oc1ccccc1C(=O)O",
+        "result": [
+         "CCCc1ccccc1CCC"
+        ],
+        "model": "claude-instant-v1",
+        "type": "mutate"
+       },
+       {
+        "left": "C[Si]1(C)C=C2OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12",
+        "result": [
+         "C[Si]1(C)C=C2OCCOC2=C1C1=CC=CCC1",
+         "OCCC1=CN=C(F)C(F)=NCCC1",
+         "C[Si]2(C=CC3OCCOC3=C2C=C)C3N=C(F)C(F)=NCCC3",
+         "CCCNC1=NC2=C(OCCO)C(CC3C)C=C3C=CCC=C12",
+         "CCC1SCOCSC1N=C(F)C(Cl)F",
+         "COC1=CC=C(C=CC=N)CN(C)CC1C1=CNC=C2OCCOC2=C1",
+         "OCCOC1=CNC2C3CCOCCOC3C2=C1C1=NC2=C(OCCO)SC2=CNC1"
+        ],
+        "model": "claude-instant-v1",
+        "type": "mutate"
+       },
+       {
+        "left": "CC(C)(C)c1ccc(O)c(c1)C(C)(C)C",
+        "right": "Cc1cccc(C=C)c1",
+        "result": "Cc1ccc(cc1)C(C)(C)c1ccc(O)c(c1)C(C)(C)c2c[nH+]c3ccc4c[nH+]c(C)(C)([O-])=NC5=C(N5)c6ccccc6",
+        "type": "add",
+        "model": "claude-instant-v1"
+       },
+       {
+        "left": "CC(C)(C)c1ccc(O)c(c1)C(C)(C)C",
+        "right": "Cc1ccc(cc1Nc2nccc(n2)c3cccnc3)NC(=O)c4ccc(cc4)CN5CCN(CC5)C",
+        "result": "c1ccc(cc1Nc2nccc(n2)c3cccnc3)NC(=O)c4ccc(cc4)CN5CCN(CC5)C \nCCCc1ccc(O)c(c1)C(C)(C)CCCNC(=O)c4ccc(cc4)CN5CCN(CC5)C",
+        "type": "add",
+        "model": "claude-instant-v1"
+       },
+       {
+        "left": "CN1CCN(Cc2ccc(C(=O)Nc3cccc(Nc4nccc(-c5cccnc5)n4)c3)cc2)CC1",
+        "right": "Cc1ccc(cc1Nc2nccc(n2)c3cccnc3)NC(=O)c4ccc(cc4)CN5CCN(CC5)C",
+        "result": "Cc1ccc(cc1Nc2nccc(n2)c3cccnc3)NC(=O)c4ccc(cc4)CN5CCN(CC5)C \nNC(=O)c1ccc(cc1CN2CCCN(Cc3ccc(C(=O)Nc4cccc(Nc5ccccn5)c4)cc3)CC2)C",
+        "type": "sub",
+        "model": "claude-instant-v1"
+       },
+       {
+        "left": "CC(C)(C)c1ccc(O)c(c1)C(C)(C)C",
+        "right": "O=C(C)Oc1ccccc1C(=O)O",
+        "result": "C1CCC2C(CCC1C)(CCC[O])OC(O)C \nCC3CC4C5C7C8O7C7CCC5C4CCC3C1CCC2C(CCC1C)(CCC[O])OC(O)C",
+        "type": "add",
+        "model": "claude-instant-v1"
+       },
+       {
+        "left": "CC(C)(C)c1ccc(O)c(c1)C(C)(C)C",
+        "right": "C[Si]1(C)C=C2OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12",
+        "result": "CC(C)(C)c1ccc(O)c(c1)C(C)(C)C \nCCC(C)(C)c1ccc(O)c(c1)C(C)(C)CC1=NSiC2=NC3=CC=C(C=O)O3C=C2NC1=O \nC1=CCC=C2C(O)=C3OCCOC3=C[Si](C)(C)C2=C1",
+        "type": "add",
+        "model": "claude-instant-v1"
+       },
+       {
+        "left": "CC(C)(C)c1ccc(O)c(C(C)(C)C)c1",
+        "right": "C[Si]1(C)C=C2OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12",
+        "result": "O=C(CO)Cc1ccccc1c2ccccc2 \nC1CCCCC1SCC1(OC)CCCC1",
+        "type": "sub",
+        "model": "claude-instant-v1"
+       },
+       {
+        "left": "Cc1cccc(C=C)c1",
+        "right": "Cc1ccc(cc1Nc2nccc(n2)c3cccnc3)NC(=O)c4ccc(cc4)CN5CCN(CC5)C",
+        "result": "Cc1cccc(C=C)c1Cc1ccc(cc1Nc2nccc(n2)c3cccnc3)NC(=O)c4ccc(cc4)CN5CCN(CC5)C",
+        "type": "add",
+        "model": "claude-instant-v1"
+       },
+       {
+        "left": "C=Cc1cccc(C)c1Cc1ccc(NC(=O)c2ccc(CN3CCN(C)CC3)cc2)cc1Nc1nccc(-c2cccnc2)n1",
+        "right": "Cc1ccc(cc1Nc2nccc(n2)c3cccnc3)NC(=O)c4ccc(cc4)CN5CCN(CC5)C",
+        "result": "Cc1ccc(cc1Nc2nccc(n2)c3cccnc3)NC(=O)c4ccc(cc4)CN5CCN(C)CC5 \nCc1ccc(cc1NC(=O)c2ccc(cnc3CCCO)cc2)Nc1nccc(-c2cccnc2)n1",
+        "type": "sub",
+        "model": "claude-instant-v1"
+       },
+       {
+        "left": "Cc1cccc(C=C)c1",
+        "right": "O=C(C)Oc1ccccc1C(=O)O",
+        "result": "CN(CCC)CCC1CCC(O)OC(=O)c1COC(=O)O",
+        "type": "add",
+        "model": "claude-instant-v1"
+       },
+       {
+        "left": "CCCN(C)CCC1CCC(O)OC(=O)C1COC(=O)O",
+        "right": "O=C(C)Oc1ccccc1C(=O)O",
+        "result": "  CC=CNC1N(C2CCC(O)OC(=O)C3COC(=O)O)CCC2CCC3",
+        "type": "sub",
+        "model": "claude-instant-v1"
+       },
+       {
+        "left": "Cc1cccc(C=C)c1",
+        "right": "C[Si]1(C)C=C2OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12",
+        "result": "C1cccc(C=C)c1 \nC[Si]1(C)C=C2OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12 \n\nCCC1COCCC1C([Si](C)(C)CC)C2NC(=O)C(F)FCN2",
+        "type": "add",
+        "model": "claude-instant-v1"
+       },
+       {
+        "left": "Cc1ccc(cc1Nc2nccc(n2)c3cccnc3)NC(=O)c4ccc(cc4)CN5CCN(CC5)C",
+        "right": "O=C(C)Oc1ccccc1C(=O)O",
+        "result": "Cc1ccc(cc1Nc2nccc(n2)c3cccnc3)NC(=O)c4ccc(cc4)CN5CCN(CC5)CO1CCOC(=O)c2ccccc12",
+        "type": "add",
+        "model": "claude-instant-v1"
+       },
+       {
+        "left": "Cc1ccc(cc1Nc2nccc(n2)c3cccnc3)NC(=O)c4ccc(cc4)CN5CCN(CC5)C",
+        "right": "C[Si]1(C)C=C2OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12",
+        "result": "Cc1ccc(cc1Nc2nccc(n2)c3cccnc3)NC(=O)c4ccc(cc4)CN5CCN(CC5)C \nCNC(=O)c1ccc(cc1Nc2nccc(n2)c3cccnc3)C[Si]1(C)C=C2OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12 \nCNC1=NC(=O)C2=C1C=CC=C3CCN(CCC3)CCOC2c4ccc(cc4)Cl1ccc2c(c(c12)NC(F)=NF)NC23CNc4c5cc(OC)cc5c4CN23",
+        "type": "add",
+        "model": "claude-instant-v1"
+       },
+       {
+        "left": "Cc1ccc(NC(=O)c2ccc(CN3CCN(C)CC3)cc2)cc1Nc1nccc(-c2cccnc2)n1",
+        "right": "C[Si]1(C)C=C2OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12",
+        "result": "C[Si]1(C)C=C2OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12 \nCc1ccc(NC(=O)c2ccc(CN3CCN(C)CC3)cc2)cc1Nc1nccc(-c2cccnc2)n1 \n\nC[Si]1(OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)]=NC12 Fc1nccc2nc(CN3CCN(C)CC3)=cc(-c2cccnc2)c1",
+        "type": "sub",
+        "model": "claude-instant-v1"
+       },
+       {
+        "left": "O=C(C)Oc1ccccc1C(=O)O",
+        "right": "C[Si]1(C)C=C2OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12",
+        "result": "O=C(C)Oc1ccccc1C(=O)OC[Si]1(C)C=C2OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12",
+        "type": "add",
+        "model": "claude-instant-v1"
+       },
+       {
+        "left": "CC(=O)Oc1ccccc1C(=O)OC[Si]1(C)C=C2OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12",
+        "right": "C[Si]1(C)C=C2OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12",
+        "result": "CCOC(=O)CCC1c2ccccc2ccc1(CN)NC",
+        "type": "sub",
+        "model": "claude-instant-v1"
+       },
+        {
+         "left": "CC(C)(C)c1ccc(O)c(c1)C(C)(C)C",
+         "result": [
+          "CC(C)(O)O"
+         ],
+         "model": "cohere-command-xlarge",
+         "type": "mutate"
+        },
+        {
+         "left": "O=C(C)Oc1ccccc1C(=O)O",
+         "result": [
+          "O=C(C)Oc1ccccc1C(=O)Oc2ccccc2"
+         ],
+         "model": "cohere-command-xlarge",
+         "type": "mutate"
+        },
+        {
+         "left": "CC(C)(C)c1ccc(O)c(c1)C(C)(C)C",
+         "right": "Cc1cccc(C=C)c1",
+         "result": "",
+         "type": "add",
+         "model": "cohere-command-xlarge"
+        },
+        {
+         "left": "",
+         "right": "Cc1cccc(C=C)c1",
+         "result": "",
+         "type": "sub",
+         "model": "cohere-command-xlarge"
+        },
+        {
+         "left": "CC(C)(C)c1ccc(O)c(c1)C(C)(C)C",
+         "right": "Cc1ccc(cc1Nc2nccc(n2)c3cccnc3)NC(=O)c4ccc(cc4)CN5CCN(CC5)C",
+         "result": "",
+         "type": "add",
+         "model": "cohere-command-xlarge"
+        },
+        {
+         "left": "",
+         "right": "Cc1ccc(cc1Nc2nccc(n2)c3cccnc3)NC(=O)c4ccc(cc4)CN5CCN(CC5)C",
+         "result": "\nThe result is\nCCC(NCNC(C(=O))C3=O)O",
+         "type": "sub",
+         "model": "cohere-command-xlarge"
+        },
+        {
+         "left": "CC(C)(C)c1ccc(O)c(c1)C(C)(C)C",
+         "right": "O=C(C)Oc1ccccc1C(=O)O",
+         "result": "",
+         "type": "add",
+         "model": "cohere-command-xlarge"
+        },
+        {
+         "left": "",
+         "right": "O=C(C)Oc1ccccc1C(=O)O",
+         "result": "c2ccccc2C(=O)Oc3ccccc3C(=O)Oc4ccccc4C(=O)Oc5ccccc5C(=O)Oc6ccccc6C(=O)Oc7ccccc7C(=O)O",
+         "type": "sub",
+         "model": "cohere-command-xlarge"
+        },
+        {
+         "left": "CC(C)(C)c1ccc(O)c(c1)C(C)(C)C",
+         "right": "C[Si]1(C)C=C2OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12",
+         "result": "",
+         "type": "add",
+         "model": "cohere-command-xlarge"
+        },
+        {
+         "left": "",
+         "right": "C[Si]1(C)C=C2OCCOC2=C1C1=CC=C(C2=C3OCCOC3=C[Si]2(C)C)C2N=C(F)C(F)=NC12",
+         "result": "from H3C-N(C(F)(C(F)(C(F)(C(F)(F)C(F)(F)(C(F)O)C2F)O)F)C-N(C2=C3C=C3O)O)(O)CH2CH2 from",
+         "type": "sub",
+         "model": "cohere-command-xlarge"
+        },
+        {
+         "left": "Cc1cccc(C=C)c1",
+         "right": "Cc1ccc(cc1Nc2nccc(n2)c3cccnc3)NC(=O)c4ccc(cc4)CN5CCN(CC5)C",
+         "result": "",
+         "type": "add",
+         "model": "cohere-command-xlarge"
+        },
+        {
+         "left": "",
+         "right": "Cc1ccc(cc1Nc2nccc(n2)c3cccnc3)NC(=O)c4ccc(cc4)CN5CCN(CC5)C",
+         "result": "",
+         "type": "sub",
+         "model": "cohere-command-xlarge"
+        },
+        {
+         "left": "Cc1cccc(C=C)c1",
+         "right": "O=C(C)Oc1ccccc1C(=O)O",
+         "result": "",
+         "type": "add",
+         "model": "cohere-command-xlarge"
+        }
 ];
